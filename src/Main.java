0e937cab8b6fd79f5259e50a511ceaa644bc378a
Muhammed Murat ACAR <57721006+MuhammedMuratACAR@users.noreply.github.com>
2021-04-27 00:56:56 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,18 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		Course course1=new Course(1,"Yaz�l�m Geli�tirici Yeti�tirme Kamp� (C# + ANGULAR)","Engin Demiro�",90);
+		Course course1=new Course(1,"Yazılım Geliştirici Yetiştirme Kampı (C# + ANGULAR)","Engin Demiroğ",90);
 		
 		Course course2=new Course();
 		course2.id=2;
-		course2.name="Yaz�l�m Geli�tirici Yeti�tirme Kamp� (JAVA + REACT)";
-		course2.instructor="Engin Demiro�";
+		course2.name="Yazılım Geliştirici Yetiştirme Kampı (JAVA + REACT)";
+		course2.instructor="Engin Demiroğ";
 		course2.progress=25;
 		
 		Course course3=new Course();
 		course3.id=3;
-		course3.name="Programlamaya Giri� i�in Temel Kurs";
-		course3.instructor="Engin Demiro�";
+		course3.name="Programlamaya Giriş için Temel Kurs";
+		course3.instructor="Engin Demiroğ";
 		course3.progress=100;
 
 		Course[] courses= {course1,course2,course3};
@@ -27,14 +27,14 @@ public class Main {
 		
 		Student student2=new Student();
 		student2.id=2;
-		student2.name="Osman �o�kun";
+		student2.name="Osman Çoşkun";
 		student2.age=20;
-		student2.city="�stanbul";
+		student2.city="İstanbul";
 		student2.mail="osmansckn@gmail.com";
 		
 		Student student3=new Student();
 		student3.id=2;
-		student3.name="Berat �etin";
+		student3.name="Berat Çetin";
 		student3.age=22;
 		student3.city="Bursa";
 		student3.mail="beratcetin@gmail.com";
@@ -42,9 +42,9 @@ public class Main {
 		Student[] students= {student1,student2,student3};
 		for (Student student : students) {
 			System.out.println(
-					"��rencinin Ad�: " + student.name +
-					" Ya��: " + student.age + 
-					" Ya�ad��� �ehir: " + student.city+
+					"Öğrencinin Adı: " + student.name +
+					" Yaşı: " + student.age + 
+					" Yaşadığı Şehir: " + student.city+
 					" Mail: " + student.mail
 					);
 		}
@@ -58,8 +58,7 @@ public class Main {
 		studentManager.studentList(student1);
 		studentManager.addToStudent(student2);
 		studentManager.deleteToStudent(student3);
-		
+
 	}
 
 }
-
